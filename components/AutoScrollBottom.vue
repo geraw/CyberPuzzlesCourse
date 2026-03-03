e3d4d590dcdf5cf630f6605960ec2aaa7d7b9476
Gera Weiss <gera.weiss@gmail.com>
2026-03-03 11:59:24 +0200

    autoscroll

---
+++ components/AutoScrollBottom.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { onMounted } from 'vue'
+
+const props = defineProps({
+  target: {
+    type: String,
+    required: true
+  },
+  delay: {
+    type: Number,
+    default: 1000
+  }
+})
+
+onMounted(() => {
+  setTimeout(() => {
+    const el = document.getElementById(props.target)
+    if (el) el.scrollTop = el.scrollHeight 
+  }, props.delay)
+})
+</script>
+
+<template>
+  <div style="display: none;"></div>
+</template>
